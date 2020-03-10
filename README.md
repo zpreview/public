1477c5ca81fb259d39f41c768d2d0941868e9164
hedgar2017 <hedgar2017@gmail.com>
2020-03-10 12:40:29 +0200

    Add the Linux installation script

--- README.md
+++ README.md
@@ -4,12 +4,14 @@
 2. Unpack its contents to some folder and add the folder to your `PATH` environment variable.
 3. Use the binaries via your favorite terminal.
 
-> **Quick setup on macOS**: Download the [macos.sh](macos.sh) Shell script and run it with `bash macos.sh` to install all the binaries (i.e. schnorr, zargo, znc, zvm) and generate a local folder that is setup with the examples ready for hacking.
+> **Quick setup on MacOS**: Download the [macos.sh](macos.sh) Shell script and run it with `bash macos.sh` to install all the binaries (i.e. schnorr, zargo, znc, zvm) and generate a local folder that is setup with the examples ready for hacking.
+
+> **Quick setup on Linux**: Download the [linux.sh](linux.sh) Shell script and run it with `bash linux.sh` to install all the binaries (i.e. schnorr, zargo, znc, zvm) and generate a local folder that is setup with the examples ready for hacking.
 
 # Documentation
 
 ZincBook: https://zinc.matterlabs.dev/
 
-# Questions
+# Gitter
 
 Please discuss here: https://gitter.im/matter-labs/zinc
--- linux.sh
+++ linux.sh
@@ -2,16 +2,16 @@
 
 # Configure version of Zinc to install from releases https://github.com/zpreview/public/releases
 VERSION="0.1.4"
-# Configure path to install the Zinc binaries (i.e. typically on macOS use "/usr/local/bin")
-ZINC_BINARIES_PATH=/tmp/zinc-$VERSION-macos/bin
+# Configure path to install the Zinc binaries (i.e. typically on Linux use "/usr/local/bin")
+ZINC_BINARIES_PATH=/tmp/zinc-$VERSION-linux/bin
 # Configure path to hack on Zinc examples
 ZINC_EXAMPLES_PATH=~/code/src/zpreview
 
 # Download and unzip Zinc binaries to configured path
 mkdir -p $ZINC_BINARIES_PATH && \
-curl -LO https://github.com/zpreview/public/releases/download/$VERSION/zinc-$VERSION-macos.zip && \
-unzip ./zinc-$VERSION-macos.zip -d $ZINC_BINARIES_PATH && \
-rm ./zinc-$VERSION-macos.zip && \
+curl -LO https://github.com/zpreview/public/releases/download/$VERSION/zinc-$VERSION-linux.tar.gz && \
+tar -xvf ./zinc-$VERSION-linux.tar.gz -C $ZINC_BINARIES_PATH && \
+rm ./zinc-$VERSION-linux.tar.gz && \
 echo "PATH='$ZINC_BINARIES_PATH:$PATH';" >> ~/.bash_profile && \
 . ~/.bash_profile && \
 
